a5b4e88d3ff9dc00a258fcf36dd18d5723055174
Jacob Somer <jacobsomer@Jacobs-MacBook-Pro.local>
2023-04-05 21:19:48 -0400

    added embeddings

--- main.py
+++ main.py
@@ -7,82 +7,85 @@ import requests
 from PyPDF2 import PdfReader
 from bs4 import BeautifulSoup
 import timeout_decorator
+from langchain.document_loaders import SeleniumURLLoader
+from langchain.document_loaders.image import UnstructuredImageLoader
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.document_loaders import UnstructuredPowerPointLoader
+from langchain.document_loaders import UnstructuredWordDocumentLoader
+from langchain.document_loaders import YoutubeLoader
+from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+import os
+from supabase import create_client, Client
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
 
 app = Flask(__name__)
 
 
-def driversetup():
-    # The following options are required to make headless Chrome
-    # work in a Docker container
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument("--window-size=1920,1080")
-    # Initialize a new browser
-    return webdriver.Chrome(options=chrome_options)
-
-
-# @timeout_decorator.timeout(5)
-def ExtractHTMLFromURL(url):
-    # try using only requests
-    try:
-        html = requests.get(url).text
-        return str(html)
-    except:
-        # first try using only requests
-        driver = driversetup()
-        driver.maximize_window()
-        # navigate to browserstack.com
-        driver.get(url)
-        html = driver.page_source
-        # close the browser
-        driver.close()
-        driver.quit()
-        return str(html)
-
-
-def extractTitleFromHTML(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    title = soup.title.string
-    text = soup.get_text()
-    return title, text
-
-
-@app.route("/getTextForPDF", methods=["POST"])
+@app.route("/createEmbeddingForText", methods=["POST"])
 @cross_origin(supports_credentials=True)
-def getTextForPDF():
+def createEmbeddingForText():
     data = request.json
     url = data["url"]
-    try:
-        response = requests.get(url)
-        with open('tmp.pdf', 'wb') as file:
-            file.write(response.content)
-        with open('tmp.pdf', 'rb') as file:
-            reader = PdfReader(file)
-            pages = [(i, page.extract_text()) for i, page in enumerate(
-                reader.pages) if page.extract_text() is not None]
-            return jsonify({
-                "pdfText": pages,
-                "url": url,
-            }), 200
-    except:
-        return jsonify({}), 400
+    text = data["text"]
+    user = data["userID"]
+    title = url.split("/")[-1]
+    response = openai.Embedding.create(
+        input=text,
+        model="text-embedding-ada-002"
+    )
+    embeddings = response['data'][0]['embedding']
+    data, _ = supabase.table('userdocuments').insert({
+        "url": url,
+        "title": title,
+        "userID": user,
+        "embeddings": embeddings
+    }).execute()
+    return jsonify(data)
 
 
-@app.route("/getTextForURL", methods=["POST"])
+@app.route("/createEmbeddingForObject", methods=["POST"])
 @cross_origin(supports_credentials=True)
-def getTextForURL():
+def createEmbeddingForObjects():
     data = request.json
     url = data["url"]
-    try:
-        html = ExtractHTMLFromURL(url)
-        title, text = extractTitleFromHTML(html)
-        return jsonify({
-            "text": text,
-            "title": title,
+    user = data["userID"]
+    title = url.split("/")[-1]
+    if url.endswith(".pdf"):
+        loader = UnstructuredPDFLoader(url)
+    elif url.endswith(".csv"):
+        loader = CSVLoader(url)
+    elif url.endswith(".jpg") or url.endswith(".png"):
+        loader = UnstructuredImageLoader(url)
+    elif url.endswith(".doc") or url.endswith(".docx"):
+        loader = UnstructuredWordDocumentLoader(url)
+    elif url.endswith(".ppt") or url.endswith(".pptx"):
+        loader = UnstructuredPowerPointLoader(url)
+    elif "youtube" in url:
+        # !pip install youtube-transcript-api
+        # ! pip install pytube
+        loader = YoutubeLoader.from_youtube_url(
+            "https://www.youtube.com/watch?v=QsYGlZkevEg", add_video_info=True)
+    else:
+        loader = SeleniumURLLoader([url])
+    data = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(data)
+    for chunk in docs:
+        response = openai.Embedding.create(
+            input="Your text string goes here",
+            model="text-embedding-ada-002"
+        )
+        embeddings = response['data'][0]['embedding']
+        data, _ = supabase.table('userdocuments').insert({
             "url": url,
-        }), 200
-    except Exception as e:
-        return jsonify({e}), 400
+            "title": title,
+            "userID": user,
+            "embeddings": embeddings
+        }).execute()
+    return jsonify(data)
