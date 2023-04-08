6e141d9ee8da2948f1c72158bfacac4a07fdd8bf
Jacob Somer <jacobsomer@Jacobs-MacBook-Pro.local>
2023-04-07 20:57:11 -0400

    update

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,19 @@
 FROM python:3.10
 
-# install google chrome
-RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-RUN echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
-RUN apt-get update && apt-get install -y google-chrome-stable
+# Allow statements and log messages to immediately appear in the Knative logs
+ENV PYTHONUNBUFFERED True
+ENV PORT 8080
+EXPOSE 8080
 
-# install chromedriver
-RUN apt-get install -yqq unzip
-RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
-RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
-
-# set display port to avoid crash
-ENV DISPLAY=:99
-
-# upgrade pip
-RUN pip install --upgrade pip
-
-# Install Python dependencies.
-COPY requirements.txt requirements.txt
-RUN pip install -r requirements.txt
-RUN pip install webdriver-manager
-
+ENV OPENAI_API_KEY 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
 
 # Copy local code to the container image.
 ENV APP_HOME /app
 WORKDIR $APP_HOME
-COPY . .
+COPY . ./
 
-# Run the web service on container startup. Here we use the gunicorn
-# webserver, with one worker process and 8 threads.
-# For environments with multiple CPU cores, increase the number of workers
-# to be equal to the cores available.
-CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 main:app
+RUN pip install --upgrade pip
+# Install production dependencies.
+RUN pip3 install -r requirements.txt
+
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
--- main.py
+++ main.py
@@ -1,51 +1,36 @@
 # main.py
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-import requests
-from PyPDF2 import PdfReader
-from bs4 import BeautifulSoup
-import timeout_decorator
 from langchain.document_loaders import SeleniumURLLoader
 from langchain.document_loaders.image import UnstructuredImageLoader
 from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.document_loaders import UnstructuredURLLoader
 from langchain.document_loaders import UnstructuredPowerPointLoader
 from langchain.document_loaders import UnstructuredWordDocumentLoader
 from langchain.document_loaders import YoutubeLoader
 from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
-import os
+from langchain.document_loaders import UnstructuredFileLoader
 from supabase import create_client, Client
+import openai
+import os
+import requests
+
+
+def download(url):
+    get_response = requests.get(url, stream=True)
+    file_name = url.split("/")[-1]
+    with open(file_name, 'wb') as f:
+        for chunk in get_response.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+    return file_name
+
 
 url: str = "https://gsaywynqkowtwhnyrehr.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzYXl3eW5xa293dHdobnlyZWhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg3NTUzMDEsImV4cCI6MTk5NDMzMTMwMX0.AFuxpyRtVjW-qcGNxuWbai8zBo9H2EDGT3JlGZKpgzc"
 supabase: Client = create_client(url, key)
 
 app = Flask(__name__)
-
-
-@app.route("/createEmbeddingForText", methods=["POST"])
-@cross_origin(supports_credentials=True)
-def createEmbeddingForText():
-    data = request.json
-    url = data["url"]
-    text = data["text"]
-    user = data["userID"]
-    title = url.split("/")[-1]
-    response = openai.Embedding.create(
-        input=text,
-        model="text-embedding-ada-002"
-    )
-    embeddings = response['data'][0]['embedding']
-    data, _ = supabase.table('userdocuments').insert({
-        "url": url,
-        "title": title,
-        "userID": user,
-        "embeddings": embeddings
-    }).execute()
-    return jsonify(data)
 
 
 @app.route("/createEmbeddingForObject", methods=["POST"])
@@ -55,36 +40,57 @@ def createEmbeddingForObjects():
     url = data["url"]
     user = data["userID"]
     title = url.split("/")[-1]
-    if url.endswith(".pdf"):
-        loader = UnstructuredPDFLoader(url)
-    elif url.endswith(".csv"):
-        loader = CSVLoader(url)
-    elif url.endswith(".jpg") or url.endswith(".png"):
-        loader = UnstructuredImageLoader(url)
-    elif url.endswith(".doc") or url.endswith(".docx"):
-        loader = UnstructuredWordDocumentLoader(url)
-    elif url.endswith(".ppt") or url.endswith(".pptx"):
-        loader = UnstructuredPowerPointLoader(url)
+
+    # download the file if its not a youtube video
+    if "youtube" not in url:
+        path = download(url)
+    if path.endswith(".pdf"):
+        loader = UnstructuredPDFLoader(path)
+    elif path.endswith(".csv"):
+        loader = CSVLoader(path)
+    elif path.endswith(".jpg") or path.endswith(".png"):
+        loader = UnstructuredImageLoader(path)
+    elif path.endswith(".doc") or path.endswith(".docx"):
+        loader = UnstructuredWordDocumentLoader(path)
+    elif path.endswith(".ppt") or path.endswith(".pptx"):
+        loader = UnstructuredPowerPointLoader(path)
     elif "youtube" in url:
-        # !pip install youtube-transcript-api
-        # ! pip install pytube
         loader = YoutubeLoader.from_youtube_url(
-            "https://www.youtube.com/watch?v=QsYGlZkevEg", add_video_info=True)
+            path, add_video_info=True)
+    elif "http" in url:
+        loader = SeleniumURLLoader([path])
     else:
-        loader = SeleniumURLLoader([url])
+        loader = UnstructuredFileLoader(path)
     data = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     docs = text_splitter.split_documents(data)
     for chunk in docs:
         response = openai.Embedding.create(
-            input="Your text string goes here",
+            input=str(chunk.page_content),
             model="text-embedding-ada-002"
         )
         embeddings = response['data'][0]['embedding']
         data, _ = supabase.table('userdocuments').insert({
             "url": url,
-            "title": title,
-            "userID": user,
-            "embeddings": embeddings
+            "userid": user,
+            "body": chunk.page_content,
+            "embedding": embeddings
         }).execute()
-    return jsonify(data)
+    return jsonify({"status": "success"}), 200
+
+
+# @app.route("/query", methods=["POST"])
+# @cross_origin(supports_credentials=True)
+# def query():
+#     data = request.json
+#     query = data["query"]
+#     user = data["userID"]
+
+#     return jsonify(response)
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(debug=True, host='0.0.0.0', port=port)
+
+# if __name__ == "__main__":
+#     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
